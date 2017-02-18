c32048afbfe5d3981fa69c44c841b44b1e177cf9
arlinmaria <arlin.maria18@gmail.com>
2017-02-18 19:01:24 +0530

    Create php13.php

---
+++ php13.php
@@ -0,0 +1,7 @@
+<?php
+$age = array("arlin"=> 24,"anu"=>23,"ammu"=>22);
+$mark = array("arlin"=> 70,"anu"=>70,"ammu"=>60);
+echo "NAME:arlin".": &nbsp AGE: ".$age['arlin']."&nbsp MARKS: ".$mark['arlin'].".<br>";
+echo "NAME:anu".": &nbsp AGE: ".$age['anu']."&nbsp MARKS: ".$mark['anu'].".<br>";
+echo "NAME:ammu".": &nbsp AGE: ".$age['ammu']."&nbsp MARKS: ".$mark['ammu'].".<br>";
+?>
